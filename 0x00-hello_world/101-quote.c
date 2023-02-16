339ba28bbd0605d65bab18299a81fdc3b354fe38
MartinOuss <oussamalamrabtii@gmail.com>
2023-02-16 13:25:10 -0800

    another fix 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,23 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints a quote without using printf or puts
+ *
+ * Return: 1 (Error code)
+ */
 int main(void)
 {
-  const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  write(2, str, 59);
-  return (1);
+    char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    int i = 0;
+
+    while (quote[i])
+    {
+        i++;
+    }
+    write(2, quote, i);
+
+    return (1);
 }
+
