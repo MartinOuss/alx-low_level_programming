ddca3341b08a6d1480223393bb4829890b1d3f26
MartinOuss <oussamalamrabtii@gmail.com>
2023-02-21 16:34:49 -0800

    anther try for 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,35 +5,25 @@
  */
 void times_table(void)
 {
-    int i, j;
+int i, j;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            int result = i * j;
+for (i = 0; i <= 9; i++)
+{for (j = 0; j <= 9; j++)
+{int result = i * j;
 
-            if (j == 0)
-            {
-                _putchar('0');
-            }
-            else if (result < 10)
-            {
-                _putchar(' ');
-                _putchar(result + '0');
-            }
-            else
-            {
-                _putchar(result / 10 + '0');
-                _putchar(result % 10 + '0');
-            }
-
-            if (j < 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-        _putchar('\n');
-    }
+if (j == 0)
+{_putchar('0');}}
+else if (result < 10){
+_putchar(' ');
+_putchar(result + '0');
+}
+else{
+_putchar(result / 10 + '0');
+_putchar(result % 10 + '0');}
+if (j < 9){
+_putchar(',');
+_putchar(' ');
+}
+_putchar('\n');
+}
 }
