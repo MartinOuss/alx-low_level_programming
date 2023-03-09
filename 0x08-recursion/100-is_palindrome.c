c9bd7f7215e5b6c64745e84e32925400293ecd3b
MartinOuss <oussamalamrabtii@gmail.com>
2023-03-09 14:30:23 -0800

    adv task should work now

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,30 +1,29 @@
 #include "main.h"
 #include <string.h>
 /**
- * _is_palindrome - recursive helper function for is_palindrome
- * @s: string to check
- * @start: starting index of the string
- * @end: ending index of the string
- *
- * Return: 1 if string is a palindrome, 0 otherwise
+* _is_palindrome - recursive helper function for is_palindrome
+* @s: string to check
+* @start: starting index of the string
+* @end: ending index of the string
+*
+* Return: 1 if string is a palindrome, 0 otherwise
 */
 int _is_palindrome(char *s, int start, int end)
 {
-  if (start >= end)
-    return (1);
-  if (s[start] != s[end])
-    return (0);
-  return (_is_palindrome(s, start + 1, end - 1));
+	if (start >= end)
+		return (1);
+	if (s[start] != s[end])
+		return (0);
+	return (_is_palindrome(s, start + 1, end - 1));
 }
-
 /**
- * is_palindrome - checks if a string is a palindrome
- * @s: string to check
- *
- * Return: 1 if string is a palindrome, 0 otherwise
+* is_palindrome - checks if a string is a palindrome
+* @s: string to check
+*
+* Return: 1 if string is a palindrome, 0 otherwise
 */
-int is_palindrome(char *s)
+int is_palindrome(char *s);
 {
-  int len = strlen(s);
-  return (_is_palindrome(s, 0, len - 1));
+	int len = strlen(s);
+		return (_is_palindrome(s, 0, len - 1));
 }
