5c0617e77e40a3c7ad43134ddb0112afa53f9231
MartinOuss <oussamalamrabtii@gmail.com>
2023-03-09 10:51:41 -0800

    fix to the last mondatory task

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,12 +8,13 @@
  */
 int is_prime_number(int n)
 {
+	int i;
+
 	if (n <= 1)
 	{
 		return (0);
 	}
-
-	for (int i = 2; i <= sqrt(n); i++)
+	for (i = 2; i <= sqrt(n); i++)
 	{
 		if (n % i == 0)
 		{
